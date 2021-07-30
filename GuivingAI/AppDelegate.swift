f9e8a2af8bbbd62516cc50dd462694c4e9becec9
JangHyun <jh@guiving.com>
2021-07-30 23:46:44 +0900

    Initial Commit

---
+++ GuivingAI/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  GuivingAI
+//
+//  Created by JangHyun on 2021/07/28.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ GuivingAI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GuivingAI
+//
+//  Created by JangHyun on 2021/07/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
